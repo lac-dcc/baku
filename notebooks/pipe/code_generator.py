4be928fdfd8e064f995fd2063978184f608eb0f2
guima <gabrielguimasr@gmail.com>
2025-02-26 14:51:23 -0300

    [FEAT] Working Chain Pipeline, but bugs founded

--- notebooks/pipe/code_generator.py
+++ notebooks/pipe/code_generator.py
@@ -5,7 +5,7 @@ import subprocess
 from data_loader import DataLoader
 
 class CodeGeneration:
-    
+
     def __init__(self,model,model_name,tokenizer,data : DataLoader,input_str: str,input_id : int,
                  max_length : int, base_code: str = "", topology="regression"):
         self.model = model
@@ -16,7 +16,7 @@ class CodeGeneration:
         self.input_id = input_id
         self.tokenizer = tokenizer
         self.base_code = base_code
-        
+
         if topology == "performance":
             self.template =  f"""
         Generate a C program optimized for the following Linux `perf` statistics:
@@ -41,35 +41,34 @@ class CodeGeneration:
             // C code here
             </Program>
         """
-        
+
         else:
              self.template = f"""
-        Complete the following code, {self.input_str}:
+        Complete the following C code, {self.input_str}:
         {self.base_code}
-        
+
 
         - **Requirements**:
             1. The program must be a completicion to the base code.
             2. Enclose the code between <Program> tags.
-
         - **Output Format**:
             <Program>
             {self.base_code}
             </Program>
         """
-            
+
         self.dif_seconds = 0
         self.ouput = ""
         self.program_name = ""
         self.program = ""
-        
-        
+
+
     def generate_code(self,response=True):#Measure time and generate the code
         input_time = time.time()
         inputs = self.tokenizer(self.template, return_tensors="pt").to(self.model.device)
         outputs = self.model.generate(**inputs, max_length=self.max_length) #model output
         output_time = time.time()
-        
+
         self.dif_seconds = (output_time - input_time) #time measure
         decoded_output = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
         self.output = decoded_output
@@ -77,56 +76,54 @@ class CodeGeneration:
         if(response):
             print('The output was:' + decoded_output)
             print(f'Time spend in the code generation: {self.dif_seconds}')
-        
-            
+
+
     def get_output_list(self):#Return the data from the program generation
         if (self.program_name == "") or (self.program == ""):
             program = self.program_extratcion()
-            
+
         if(self.base_code in self.program):#Do not accept base code as a generated code
-                    
+
             len_program = len(self.tokenizer.encode(self.program)) #count the amount of tokens in the program
-            return [self.model_name, 
-                    self.dif_seconds, 
+            return [self.model_name,
+                    self.dif_seconds,
                     self.program_name,
                     self.input_id,
                     -1]
-        
-        gpu_name = self.get_gpu_name()
+
         len_program = len(self.tokenizer.encode(self.program)) #count the amount of tokens in the program
-        return [self.model_name, 
-                self.dif_seconds, 
+        return [self.model_name,
+                self.dif_seconds,
                 self.program_name,
-                self.input_id, 
+                self.input_id,
                 len_program]
-   
-            
-    def program_extratcion(self):#Return the program generated
+
+
+    def program_extratcion(self):#Return the program generated"
         self.name_generator()
-        
+
         if self.program == "":
             final_program = ""
             suboutput_list = self.output.split("<Program>")
-            
+
             for subprogram in (suboutput_list):
                 program = subprogram.split("</Program>")[0]
 
-                if(program.find("}")!=-1):
-                    final_program = program
-            
-            self.program = final_program 
-        
+                if((program.count('{') + program.count('}')%2==0)):
+                        final_program = program
+
+            self.program = final_program
+
         return self.program
 
-    
+
     def name_generator(self,size=4): #Randomly generate a new file name
-        
+
         if self.program_name == "" :
             chars=string.ascii_uppercase + string.digits
             random_name = ''.join(random.choice(chars) for _ in range(size))
             while random_name in self.data.column_values('code_name'):
                 random_name = ''.join(random.choice(chars) for _ in range(size))
             self.program_name = random_name
-        
-        
-        
+
+
--- notebooks/pipe/generation_pipeline.py
+++ notebooks/pipe/generation_pipeline.py
@@ -6,7 +6,6 @@ from data_loader import DataLoader
 from code_generator import CodeGeneration
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUploadclass 
-
 class GenerationPipeline:
     def __init__(
             self,
@@ -20,7 +19,7 @@ class GenerationPipeline:
             chains_data_path: str,
             topology: str = 'regression'  # Nome corrigido
         ):
-        self.model = model 
+        self.model = model
         self.model_name = model_name
         self.tokenizer = tokenizer
         self.input_data = DataLoader(input_data_path)
@@ -28,15 +27,15 @@ class GenerationPipeline:
         self.sampling_data = DataLoader(sampling_data_path)
         self.models_chain = DataLoader(models_chain_path)
         self.chains_data = DataLoader(chains_data_path)
-        
+
         if topology == "performance":
             self.ignore_coluns = ['Suite','Program','id']
-        
+
         else:
             self.ignore_coluns = ['id','type']
-            
+
         self.topology = topology
-        
+
     def upload_single_file_to_drive(self,code_name,program):#Upload file to drive for colab
         try:
             folder_id = '1y_BZ7M_Rpq7q4TEoRQiGMkfDWhR0okan'
@@ -58,8 +57,8 @@ class GenerationPipeline:
 
             print(f"File uploaded successfully. File ID: {uploaded_file.get('id')}")
         except Exception as e:
-            print(f"File could not be uploaded. ERROR: {e}")  
-            
+            print(f"File could not be uploaded. ERROR: {e}")
+
     def upload_chain_file_to_drive(self,program,folder_id,folder_name,n):#Upload file to drive for colab
         try:
             local_file_path = f"/content/{folder_name}{n}.c"
@@ -80,79 +79,79 @@ class GenerationPipeline:
 
             print(f"File uploaded successfully. File ID: {uploaded_file.get('id')}")
         except Exception as e:
-            print(f"File could not be uploaded. ERROR: {e}")  
-    
-    
+            print(f"File could not be uploaded. ERROR: {e}")
+
+
     def folder_generator(self,parent_folder_id,size=4):
         chars=string.ascii_uppercase
         random_name = ''.join(random.choice(chars) for _ in range(size))
         while random_name in self.chains_data.column_values('folder_id'):
             random_name = ''.join(random.choice(chars) for _ in range(size))
-        
+
         folder_metadata = {
             'name': random_name,
             'mimeType': 'application/vnd.google-apps.folder'
         }
-        
+
         if parent_folder_id:
             folder_metadata['parents'] = [parent_folder_id]
-        
+
         service = build('drive', 'v3')
         created_folder = service.files().create(
             body=folder_metadata,
             fields='id'
         ).execute()
-        
+
         return created_folder.get('id'),random_name
-      
-        
-    
+
+
+
     def single_code_generation(self, max_len: int, static: bool = False, id: int = 0) -> float:#Single Program Generation
         try:
             input_value = ""
-            
+
             if static:#Static Value
                 input_value,input_id = self.input_data.static_row_string(id,self.ignore_coluns)
-            
+
             else:#Random
                 input_value,input_id = self.input_data.random_row_string(self.ignore_coluns)
-            
-            
+
+
             base_code = """ int f(int a) {
                 return 0;
                 }"""
-            
+
             generator = CodeGeneration(
             model=self.model,
             model_name=self.model_name,
             tokenizer=self.tokenizer,
-            data=self.models_data,  
+            data=self.models_data,
             input_str=input_value,
             input_id=input_id,
             max_length=max_len,
             base_code=base_code,
-            topology=self.topology  
+            topology=self.topology
             )
-            
+
             generator.generate_code()
-            
+
             program = generator.program_extratcion()
-            
+
             output_list = generator.get_output_list()
-            
+
             if(output_list[4] > 0):
                 self.upload_single_file_to_drive(output_list[2],program)#Save generation
-        
+
             self.models_data.new_row(output_list)
             self.models_data.save()
-            
+
             print("Program Generated:\n" + program)
-            return output_list[1] 
+            return output_list[1]
         except Exception as e:
             print(f"ERRO: {e}")
 
-   
-   
+
+
     def single_sampling(self,first_max_length=1024,max_lenght_pow=1,sample=12):#Simulate different generation for time measure
 
         current_max_length = first_max_length#Begin with some length
@@ -182,8 +181,8 @@ class GenerationPipeline:
 
             current_max_length *= 2 #make it double beacause could grow better
 
-        print("Sampling Done!")     
-    
+        print("Sampling Done!")
+
     def chain_code_generation(self, max_len: int, n: int = 1) -> float:
         try:
             ac_time = 0
@@ -192,52 +191,59 @@ class GenerationPipeline:
                     }"""
             i = 0
             last_code_id = ''
+            folder_id,folder_name = self.folder_generator('1y_BZ7M_Rpq7q4TEoRQiGMkfDWhR0okan')
             for _ in range(n):
                 input_value,input_id = self.input_data.random_row_string(self.ignore_coluns)
-            
+
                 generator = CodeGeneration(
                 model=self.model,
                 model_name=self.model_name,
                 tokenizer=self.tokenizer,
-                data=self.models_data,  
+                data=self.models_data,
                 input_str=input_value,
                 input_id=input_id,
                 max_length=max_len,
                 base_code=base_code,
-                topology=self.topology  
+                topology=self.topology
                 )
-                
+
                 generator.generate_code()
-                
+
                 program = generator.program_extratcion()
-                
+
                 output_list = generator.get_output_list()
-                
-                if i==0:
-                    folder_id,folder_name = self.folder_generator('1y_BZ7M_Rpq7q4TEoRQiGMkfDWhR0okan')
-                
+
+
                 if(output_list[4] > 0):
+                    if i==0:
+                        last_code_id = 'None'
+                    else:
+                      last_code_id = f'{folder_name}{i-1}'
+
+                    i = i + 1
+
                     output_list.append(last_code_id)
                     output_list.append(folder_name)
+                    output_list[2] = f'{folder_name}{i}'
+
                     self.models_chain.new_row(output_list)
                     self.models_chain.save()
-                    
-                    
-                    last_code_id = f'{folder_name}{i}'
+
+
+                    last_code_id = f'{folder_name}{i-1}'
                     self.upload_chain_file_to_drive(program,folder_id,folder_name,i)
-                    
-                    i = i + 1
-                    
-                    ac_time = ac_time + output_list[1] 
+
+
+                    ac_time = ac_time + output_list[1]
                     base_code = program
-                    
+
                     print("Program Generated:\n" + program)
-               
+
             #folder_id,num_regression,time_spent,mean_time_program,std_time,compilation
-            rows_chain = [folder_id,0,ac_time,0,0,"False"]    
+            rows_chain = [folder_name,0,ac_time,0,0,"False"]
             self.chains_data.new_row(rows_chain)
             self.chains_data.save()
-            
+
             return ac_time
         except Exception as e:
             print(f"ERRO: {e}")
@@ -245,4 +251,3 @@ class GenerationPipeline:
     def chain_sampling():
         return 0
         #empty for now
-        
