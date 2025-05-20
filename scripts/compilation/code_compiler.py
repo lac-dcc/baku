802755fbd0f112c18d097126ed3fca29876fae37
gasa12337@gmail.com <gasa12337@gmail.com>
2025-05-19 21:27:36 -0300

    [FIX] Only one compiler file to run now

--- scripts/compilation/code_compiler.py
+++ scripts/compilation/code_compiler.py
@@ -1,39 +1,191 @@
 import sys
 import os
+import time
 import subprocess
+import shutil
+sys.path.append("../classes")
+from data_loader import DataLoader
 
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: python code_compiler.py <file> <optimizer>")
-        sys.exit(1)
+# TODO: 1. Test diff versions  3. Test clang vs gcc 
 
-    file = sys.argv[1]
-    optimizer = sys.argv[2]
+def get_binary_size(file_path):
+    if not os.path.exists(file_path):
+        return 0
+    try:
+        result = subprocess.run(['size', file_path], capture_output=True, text=True, check=True)
+        lines = result.stdout.splitlines()
+        
+        if len(lines) < 2:
+            return 0
+        
+        parts = lines[1].split()
 
-    if not os.path.isfile(file):
+        return int(parts[0])
+    
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao executar o comando 'size': {e.stderr}")
+        return 0
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+        return 0
+
+def has_main_function(file_path):
+    with open(file_path, "r") as f:
+        content = f.read()
+    return "int main" in content or "void main" in content
+
+def get_compilable_code(folder,file):
+    file_read = open(folder+'/'+file, "r+")
+    file_content = file_read.read()
+    file_read.close()
+
+    main_template = """
+    void main(){
+        printf("%d",f(7));
+    }
+    """
+
+    code= "#include<stdio.h> \n"+file_content+main_template
+
+    file_write = open(folder+'/'+file, "w")
+    file_write.write(code)
+    file_write.close()
+
+def get_available_gcc_versions():
+    versions = []
+    try:
+       
+        result = subprocess.run(["compgen", "-c", "gcc-"], capture_output=True, text=True, shell=True)
+        if result.returncode == 0:
+            versions = [v for v in result.stdout.split() if v.startswith('gcc-') and v[4].isdigit()]
+        
+        
+        result = subprocess.run(["gcc", "--version"], capture_output=True, text=True)
+        if result.returncode == 0:
+            versions.append("gcc")  
+    except Exception as e:
+        print(f"Erro: {e}")
+    
+    return sorted(list(set(versions)))
+
+def compilation(compiler, version, optimizer, folder, file, compiler_path,chain_type):
+    completition = ''
+
+    if(chain_type):
+        completition='chain'
+    else:
+        completition='singles'
+
+    compilation_df = DataLoader(f'{compiler_path}compilation_{completition}.csv')
+    
+    if not os.path.isfile(folder+'/'+file):
         print(f"File '{file}' not found.")
         sys.exit(1)
 
-    output = file.replace(".c", ".out")
-
-    opt_flags = ["-O0", "-O1", "-O2", "-O3", "-Os", "-Ofast"]
-
-    if optimizer not in opt_flags:
-        print(f"WARNING: Invalid optimizer flag '{optimizer} (using -O0)'.")
-        print("Valid flags are: -O0, -O1, -O2, -O3, -Os, -Ofast")
+    if not has_main_function(folder+'/'+file):
+        get_compilable_code(folder, file)
+    
+    output = file.replace(".c", f".{version}.out")
 
     try:
-        result = subprocess.run(["gcc", optimizer, file, "-o", output],
-                                capture_output=True, text=True)
+        bin_array = []
+        time_array = []
+        for _ in range(10):
+            cmd = [compiler] if version == "default" else [f"{compiler}-{version}"]
+            cmd.extend([optimizer, folder+'/'+file, "-o", folder+'/output/'+output])
+            
+            begin_time = time.time()
+            result = subprocess.run(cmd, capture_output=True, text=True)
+            end_time = time.time()
 
-        if result.returncode != 0:
-            print(f"Error compiling '{file}':")
-            print(result.stderr)
-            # TODO: Register the file as not compiled in the data
-            sys.exit(1)
+            time_array.append(abs(end_time - begin_time))
+
+            if result.returncode != 0:
+                print(f"Error compiling '{file}' with {compiler} {version}:")
+                print(result.stderr)
+                return False
+            else:
+                print(f"Compiled '{file}' to '{output}' -> {compiler}-{version} {optimizer}")
+                bin_size = get_binary_size(folder+'/output/'+output)
+                bin_array.append(bin_size)
+                print(f"Binary Size: {bin_size}")
+        
+        min_bin = min(bin_array)
+        max_bin = max(bin_array)
+        mean = sum(bin_array) / len(bin_array)
+        time_spent = sum(time_array)
+
+        if(chain_type):
+            row = [folder.split('/')[5], file, f"{compiler}",f"{version}", optimizer, min_bin, max_bin, mean,time_spent]
+            compilation_df.new_row(row)
+            compilation_df.save()
         else:
-            print(f"Compiled '{file}' to '{output}'")
+            row = [file.split('.')[0], f"{compiler}",f"{version}", optimizer, min_bin, max_bin, mean,time_spent]
+            compilation_df.new_row(row)
+            compilation_df.save()
 
+        return True
     except Exception as e:
         print(f"Unexpected error: {e}")
         sys.exit(1)
+
+def main():
+    if len(sys.argv) < 6:
+        print("Usage: python chain_code_compiler.py <file_folder> <data_path> <string chain_or_single> <model_name> <size> ")
+        sys.exit(1)
+
+    file_folder = sys.argv[1]
+    compiler_path = sys.argv[2]
+    chain = sys.argv[3]
+    model_name = sys.argv[4]
+    size = sys.argv[5]
+
+    chain_type = True
+
+    if "single" in chain:
+        chain_type = False
+
+    if not os.path.isdir(file_folder):
+        print(f"Folder '{file_folder}' not found.")
+        sys.exit(1)
+
+    COMPILERS = {
+        "gcc": ["12"]
+    }
+
+    OPT_FLAGS = ["-O0", "-O1", "-O2", "-O3", "-Os", "-Ofast"]
+
+    run = 0
+    total = 0
+
+    for folder, _, files in os.walk(file_folder):
+        files.sort()
+        if not os.path.exists(folder+"/output"):
+            os.makedirs(folder+"/output")
+        elif os.path.exists(folder+"/output"):
+            shutil.rmtree(folder+"/output")
+            os.makedirs(folder+"/output")
+        for file in files:
+            total = total + 1
+            compiled = False
+            for compiler, versions in COMPILERS.items():
+                for version in versions:
+                    for opt in OPT_FLAGS:
+                        print(f"File:{file} - Compiler: {compiler}-{version} - Opt: {opt}")
+                        compiled = compilation(compiler, version, opt, folder, file, compiler_path,chain_type)
+            if compiled: 
+                run = run + 1
+
+    if(chain_type):
+        cm = DataLoader("../../data/TimeCodeExperiment/compiled_chain_models.csv")
+        cm.new_row([model_name,size,run, total, run/total])
+        cm.save()
+    
+    else:
+        cm = DataLoader("../../data/TimeCodeExperiment/compiled_single_models.csv")
+        cm.new_row([model_name,size,run, total, run/total])
+        cm.save()
+
+
+if __name__ == "__main__":
+    main()
